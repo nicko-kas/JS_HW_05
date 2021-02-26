2b52ed9f0a6d6c8749363c034d317389b1cb2d94
Nikolay Kasianiuk <kasyanuck@gmail.com>
2021-02-26 15:15:20 +0200

    extra_hw

--- additional_hw/task-1.js
+++ additional_hw/task-1.js
@@ -3,40 +3,85 @@
 // <p>Цей клас буде мати такі методи:</p>
 // <ol>
 class Calculator {
-constructor(a,b,result){
-  this.a=a
-  this.b=b
-  this.result = result
-}
-//<li>Метод що запитує в користувача 2 числа</li>
-inputRequest (a,b){
-  this.a = a
-  this.b = b
+  result = null;
+
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+  //<li>Метод що запитує в користувача 2 числа</li>
+  inputRequest(a, b) {
+    //this.a = prompt("Введите первое число");
+    //this.b = prompt("Введите второе число");
+    this.a = a;
+    this.b = b;
+  }
+  sum() {
+    this.result = this.a + this.b;
+    this.getResult();
+  }
+  minus() {
+    this.result = this.a - this.b;
+    this.getResult();
+  }
+  mult() {
+    this.result = this.a * this.b;
+    this.getResult();
+  }
+  division() {
+    this.result = this.a / this.b;
+    this.getResult();
+  }
+
+  getResult() {
+    console.log(`Результат операції ${this.result}`);
+  }
+
+  getNewFunc(callback) {
+    this.result = callback.call(this, this.a, this.b);
+    this.getResult();
+  }
 }
 
+const calc = new Calculator(0, 0);
 
-}
-  
-  <li>
-    Метод що додає ці числа
-  </li>
-  <li>Метод що віднімає ці числа</li>
-  <li>Метод що перемножує ці числа</li>
-  <li>Метод що ділить ці числа</li>
-  <li>
-    Метод що виводить результат арефметичної операції на екран в
-    форматі <code> `Результат операції ${value}`</code>
-    <br />
-    цей метод запускаєтьсяв кінці кожного з методів 2-5
-  </li>
-  <li>
-    Метод що додає в калькулятор новий функціонал (Приймає
-    аргументом колбек в якому описаний новий метод)
-  </li>
-  <li>
-    Передати в метод №7 колбек ф-ю що підносить числа в степінь
-  </li>
-  <li>Написати всі методи і перевірити чи вони працюють</li>
-</ol>
-</li>
-<li>
+calc.inputRequest(3, 3);
+console.log("mult");
+calc.mult();
+console.log("sum");
+calc.sum();
+console.log("division");
+calc.division();
+console.log("minus");
+calc.minus();
+
+console.log("power");
+// calc.getNewFunc(power(bind(calc).a, power.bind(calc).b));
+calc.getNewFunc((a, b) => {
+  return Math.pow(a, b);
+});
+
+//   //
+//   <li>
+//     Метод що додає ці числа
+//   </li>
+//   <li>Метод що віднімає ці числа</li>
+//   <li>Метод що перемножує ці числа</li>
+//   <li>Метод що ділить ці числа</li>
+//   <li>
+//     Метод що виводить результат арефметичної операції на екран в
+//     форматі <code> `Результат операції ${value}`</code>
+//     <br />
+//     цей метод запускаєтьсяв кінці кожного з методів 2-5
+//   </li>
+//   <li>
+//     Метод що додає в калькулятор новий функціонал (Приймає
+//     аргументом колбек в якому описаний новий метод)
+//   </li>
+//   <li>
+//     Передати в метод №7 колбек ф-ю що підносить числа в степінь
+//   </li>
+//   <li>Написати всі методи і перевірити чи вони працюють</li>
+// </ol>
+// </li>
+// <li>
--- additional_hw/task-2.js
+++ additional_hw/task-2.js
@@ -1,32 +1,169 @@
-// Напиши класс User для создания пользователя со следующим свойствами:
-// name - строка
-// age - число
-// followers - число
-// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+// {
+//   /* <p>Створити класи Library та Book</p> */
+// }
+// {
+//   /* <ol>
+//   <li>
+//     Клас Library буде описувати нашу бібліотеку книжок. Всі книжки
+//     будуть зберігатися в масиві.
+//     <p>Також цей клас буде мати такі методи:</p>
+//     <ul>
+class Library {
+  books = [];
+  favorites = [];
 
-class User {
-  constructor({ name, age, followers }) {
-    this.name = name;
-    this.age = age;
-    this.followers = followers;
+  // }
+  // Метод <strong>buy</strong> - це метод що приймає аргументом
+  // обєкт книги і додає її в бібліотеку якщо її там ще немає.
+  // Якщо книга вже є показує повідомлення що книга вже куплена
+
+  buy(book) {
+    if (!this.books.includes(book)) {
+      this.books.push(book);
+    } else {
+      return console.log(`книга вже куплена`);
+    }
   }
-  getInfo() {
-    return console.log(
-      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+
+  // Метод <strong>sell</strong> - це метод що приймає аргументом
+  // назву книги і видаляє її з бібліотеки
+
+  sell(book) {
+    this.books = this.books.filter((el) => {
+      return el.title !== book;
+    });
+  }
+  //         Метод <strong>addToFavourite</strong> - це метод що приймає
+  //         аргументом назву книги і додає обєкт книги в список
+  //         улюблених
+  //       </li>
+  //       <li>
+  addToFavourite(book) {
+    this.favorites.push(
+      this.books.find((el) => {
+        return el.title == book;
+      })
     );
+  }
+
+  // Метод <strong>removeFromFavourite</strong> - це метод що
+  //         приймає назву книги і видаляє її з списку улюблених
+  removeFromFavourite(book) {
+    this.favorites = this.favorites.filter((el) => {
+      console.log(el);
+      return el.title !== book;
+    });monitoring.submit
+  }
+
+  //         Гетер <strong>сountFavouriteBooks</strong> - це гетер що
+  //         показує кількість книжок що додані в улюблені
+
+  get сountFavouriteBooks() {
+    return this.favorites.length;
+  }
+  //         Гетер <strong>finishedBook</strong> - це гетер що містить
+  //         колекцію всіх прочитаних книжок
+  get finishedBooks() {
+    return;
   }
 }
 
-const mango = new User({
-  name: "Mango",
-  age: 2,
-  followers: 20,
+const lib = new Library();
+
+//         Метод <strong>totalCost</strong> - це метод що рахує
+//         вартість всіх книжок в бібліотеці
+//         <br />
+//         <br />
+//       </li>
+//     </ul>
+//   </li>
+//   // <li>
+//   //   Клас Book буде описувати окрему книгу. Кожна книга це буде обєкт
+//   //   з такими ключами
+//   //   <ul>
+class Book {
+  constructor({ author, title, price, totalPages, currentPage = 0 }) {
+    this.author = author;
+    this.title = title;
+    this.price = price;
+    this.totalPages = totalPages;
+    this.currentPage = currentPage;
+  }
+
+  //   Метод <strong>read</strong> - це метод що приймає кількість
+  //   сторінок що було прочитано і міняє значення currentPage
+
+  read(page) {
+    this.currentPage += page;
+  }
+
+  //         Гетер <strong>bookProgres</strong> - гетер що переводить
+  //         кількість прочитаних сторінок в відсотки
+  get bookProgress() {
+    return (this.currentPage / this.totalPages) * 100;
+  }
+
+  //         Сетер <strong>bookProgres</strong> - сетер що переводить
+  //         відсоток прочитаних сторінок в кількість
+  set bookProgress(percent) {
+    this.currentPage = this.totalPages * (percent / 100);
+  }
+}
+
+const kobzar = new Book({
+  author: "shevchenko",
+  title: "kobzar",
+  price: 100,
+  totalPages: 500,
 });
 
-mango.getInfo(); // User Mango is 2 years old and has 20 followers
-const poly = new User({
-  name: "Poly",
-  age: 3,
-  followers: 17,
+const azbuka = new Book({
+  author: "noname",
+  title: "azbuka",
+  price: 200,
+  totalPages: 10,
 });
-poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+const green = new Book({
+  author: "green",
+  title: "green",
+  price: 400,
+  totalPages: 450,
+});
+
+kobzar.read(25);
+kobzar.read(5);
+console.log(kobzar.currentPage);
+console.log(kobzar.bookProgress);
+
+kobzar.bookProgress = 50;
+console.log(kobzar.currentPage);
+
+lib.buy(kobzar);
+lib.buy(azbuka);
+lib.buy(green);
+lib.buy(green);
+
+lib.sell("kobzar");
+
+lib.addToFavourite("green");
+lib.addToFavourite("azbuka");
+console.log(lib.сountFavouriteBooks);
+console.log(lib);
+lib.removeFromFavourite("green");
+console.log(lib);
+
+// console.log(
+//   lib.books.filter((el) => {
+//     return el.title !== "kobzar";
+//   })
+// );
+
+//       Клас Book приймає один аргумент - обєкт з парметрами книги. В
+//       середині класу використовувати деструктуризацію обєкта
+//     </p>
+//   </li>
+// </ol>
+// </li>
+// </ol>
+// </article>
